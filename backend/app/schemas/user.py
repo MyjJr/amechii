18411da6a238e83905f443499589414c970832b2
Kuroi_Cc <ChenchengMiKuRo@gmail.com>
2021-09-01 15:59:57 +0900

    basic user api model

---
+++ backend/app/schemas/user.py
@@ -0,0 +1,24 @@
+from datetime import datetime
+from typing import Optional
+from pydantic import BaseModel
+
+
+class UserBase(BaseModel):
+    name: Optional[str] = None
+
+
+class UserCreate(UserBase):
+    name: str
+    password: str
+
+
+class UserUpdate(UserBase):
+    password: Optional[str] = None
+
+
+class User(UserBase):
+    id: Optional[int] = None
+    registration_time: Optional[datetime] = None
+
+    class Config:
+        orm_mode = True
