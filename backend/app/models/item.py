2bbde81c295c247a0d0192f939aeea46c0d2b8d8
Kuroi_Cc <ChenchengMiKuRo@gmail.com>
2021-09-01 15:58:48 +0900

    DB basic item model

---
+++ backend/app/models/item.py
@@ -0,0 +1,12 @@
+from sqlalchemy import Column, Integer, String
+
+from app.db.base_class import Base
+
+
+class Item(Base):
+    __tablename__ = "itms"
+
+    id = Column(Integer, primary_key=True, index=True, autoincrement=True)
+    name = Column(String(256), nullable=False)
+    image = Column(String(256))
+    price = Column(Integer, nullable=False)
