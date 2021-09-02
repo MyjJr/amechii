4d8962df4e9b6c1dbdc034f6beeb0c23b7efb12d
Kuroi_Cc <ChenchengMiKuRo@gmail.com>
2021-09-03 04:31:43 +0900

    use request.state.user from middleware

--- backend/app/api/api_v1/endpoints/users.py
+++ backend/app/api/api_v1/endpoints/users.py
@@ -1,16 +1,16 @@
 from fastapi import APIRouter, Depends
-from sqlalchemy.orm import Session
+# from sqlalchemy.orm import Session
 
-from app.api.utils.db import get_db
-from app import crud
+# from app.api.utils.db import get_db
+from app.schemas.user import User
+from app.models.user import User as DBUser
+from app.api.utils.security import get_current_user
 
 router = APIRouter()
 
 
-@router.get("/testuser")
+@router.get("/testuser", response_model=User)
 async def get_test_user(  # yapf: disable
-    db: Session = Depends(get_db),
+    current_user: DBUser = Depends(get_current_user)
 ):
-    res = crud.user.get(db, 1)
-    print(res)
-    return res
+    return current_user
