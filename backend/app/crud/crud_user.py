a083545197e41c502f2a12cfb2874aa8b19c630a
Kuroi_Cc <ChenchengMiKuRo@gmail.com>
2021-09-02 19:52:45 +0900

    useing configlocal to test

--- backend/app/crud/crud_user.py
+++ backend/app/crud/crud_user.py
@@ -1,6 +1,3 @@
-# import sys
-# sys.path.append("./backend")
-
 from sqlalchemy.orm import Session
 
 from app.crud.base import CRUDBase
@@ -23,9 +20,10 @@ user = CRUDUser(User)
 if __name__ == "__main__":
     from sqlalchemy import create_engine
     from sqlalchemy.orm import sessionmaker
-    url = "mysql+pymysql://fastapi:secret@127.0.0.1:3306/fastapi"
-    engine = create_engine(url, encoding='UTF-8', echo=True)
+    from app.core import configlocal
+    engine = create_engine(configlocal.SQLALCHEMY_DATABASE_URI, encoding='UTF-8', echo=True)
     session = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
     db: Session = session()
     t = db.query(User).all()
-    print(t)
+    print(t[0].id)
