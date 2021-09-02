c97f8141c12d4b45037a8f0d9e9cbb19537bb378
Kuroi_Cc <ChenchengMiKuRo@gmail.com>
2021-09-03 02:16:18 +0900

    Update user DB model

--- backend/app/models/user.py
+++ backend/app/models/user.py
@@ -12,6 +12,6 @@ class User(Base):
     id = Column(Integer, primary_key=True, index=True, autoincrement=True)
     name = Column(String(32), index=True, nullable=False)
     password = Column(String(64), nullable=False)
-    registration_time = Column(DateTime, default=datetime.datetime.now)
+    registration_time = Column(DateTime, default=datetime.datetime.now())
 
     tasks = relationship("Task", backref="users")
