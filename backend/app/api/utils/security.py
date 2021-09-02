60eb6d8093a4fa96e0fd69b89a07c525943cf78c
Kuroi_Cc <ChenchengMiKuRo@gmail.com>
2021-09-03 04:32:32 +0900

    simplified code

--- backend/app/api/utils/security.py
+++ backend/app/api/utils/security.py
@@ -1,35 +1,14 @@
 from sqlalchemy.orm import Session
-from fastapi import Depends, Security, HTTPException
-from fastapi.security import OAuth2PasswordBearer
-from jose import JWTError, jwt
-from starlette.status import HTTP_403_FORBIDDEN
+from fastapi import Depends, Security
+from starlette.requests import Request
 
-from app import crud
-from app.core import config
-from app.schemas.token import TokenPayload
-from app.core.jwt import ALGORITHM
+from app.core.jwt import validate_token, reusable_oauth2
 from app.api.utils.db import get_db
 
-reusable_oauth2 = OAuth2PasswordBearer(tokenUrl=config.API_ROOT_PATH + "/login/access-token")  # yapf: disable
+
+def get_current_user(request: Request):
+    return request.state.user
 
 
-def get_current_user(
-    db: Session = Depends(get_db), token: str = Security(reusable_oauth2)
-):
-    credentials_exception = HTTPException(
-        status_code=HTTP_403_FORBIDDEN,
-        detail="Could not validate credentials",
-        headers={"WWW-Authenticate": "Bearer"},
-    )
-
-    try:
-        payload = jwt.decode(token, config.SECRET_KEY, algorithms=[ALGORITHM])
-        print("\n\n\n\n", payload)
-        token_data = TokenPayload(**payload)
-        print("\n\n\n\n", token_data.user_id)
-    except JWTError:
-        raise credentials_exception
-    user = crud.user.get(db, id=token_data.user_id)
-    if not user:
-        raise HTTPException(status_code=404, detail="User not found")
-    return user
+def get_validated_current_user(db: Session = Depends(get_db), token: str = Security(reusable_oauth2)):  # yapf: disable
+    return validate_token(db, token)
