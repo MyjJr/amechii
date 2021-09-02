152ba3960a7f506b9595e24d88244024b08389f6
Kuroi_Cc <ChenchengMiKuRo@gmail.com>
2021-09-02 22:48:46 +0900

    add method get_by_name in User

--- backend/app/crud/crud_user.py
+++ backend/app/crud/crud_user.py
@@ -1,4 +1,5 @@
 from sqlalchemy.orm import Session
+from typing import Optional
 
 from app.crud.base import CRUDBase
 from app.models.user import User
@@ -14,6 +15,9 @@ class CRUDUser(CRUDBase[User, UserCreate, UserUpdate]):
         db_session.refresh(db_obj)
         return db_obj
 
+    def get_by_name(self, db_session: Session, *, name: str) -> Optional[User]:
+        return db_session.query(User).filter(User.name == name).first()
+
 
 user = CRUDUser(User)
 
@@ -21,7 +25,9 @@ if __name__ == "__main__":
     from sqlalchemy import create_engine
     from sqlalchemy.orm import sessionmaker
     from app.core import configlocal
-    engine = create_engine(configlocal.SQLALCHEMY_DATABASE_URI, encoding='UTF-8', echo=True)
+    engine = create_engine(
+        configlocal.SQLALCHEMY_DATABASE_URI, encoding='UTF-8', echo=True
+    )
     session = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
     db: Session = session()
