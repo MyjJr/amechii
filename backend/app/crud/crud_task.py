74b43b72f925df1e190b564be60f97dbcb8c922d
Kuroi_Cc <ChenchengMiKuRo@gmail.com>
2021-09-04 23:40:46 +0900

    fix bug

--- backend/app/crud/crud_task.py
+++ backend/app/crud/crud_task.py
@@ -1,3 +1,4 @@
+from typing import Optional
 from sqlalchemy.orm import Session
 
 from app.crud.base import CRUDBase
@@ -6,11 +7,11 @@ from app.schemas.task import TaskCreate, TaskUpdate
 
 
 class CRUDTask(CRUDBase[Task, TaskCreate, TaskUpdate]):
-    def create(self, db_session: Session, *, obj_in: TaskCreate) -> Task:
+    def create(self, db_session: Session, *, obj_in: TaskCreate) -> Optional[Task]:
         if not obj_in.do_id:
             obj_in.do_id = obj_in.set_id
 
-        return super().create(db_session, obj_in)
+        return super().create(db_session, obj_in=obj_in)
 
 
 task = CRUDTask(Task)
