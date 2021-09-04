422ab32b5165c24be52076bc44d43fe6f053c771
Kuroi_Cc <ChenchengMiKuRo@gmail.com>
2021-09-04 15:03:58 +0900

    implement user table

--- backend/app/models/user.py
+++ backend/app/models/user.py
@@ -1,12 +1,11 @@
-from typing import TYPE_CHECKING
+# from typing import TYPE_CHECKING
 from sqlalchemy import Column, Integer, String, DateTime, Table, ForeignKey
 from sqlalchemy.orm import relationship
 import datetime
 from app.db.base_class import Base
 
-
-if TYPE_CHECKING:
-    from app.models.task import Task
+# if TYPE_CHECKING:
+#     from app.models.task import Task
 
 user_following = Table(
     "user_following", Base.metadata,
@@ -14,15 +13,18 @@ user_following = Table(
     Column("follwing_id", Integer, ForeignKey("users.id"), nullable=False)
 )
 
+
 class User(Base):
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True, index=True, autoincrement=True)
     name = Column(String(32), index=True, nullable=False)
+    display_name = Column(String(32), index=True, nullable=False)
+    icon = Column(String(256), nullable=False)
     password = Column(String(64), nullable=False)
     registration_time = Column(DateTime, default=datetime.datetime.now())
 
-    tasks = relationship("Task", back_populates="users")
+    # tasks = relationship("Task", back_populates="users")
 
     following = relationship(
         "User",
