0b7e8616c5a414d1b20e5018baa05acedb6f9687
zookam <yuzukamonobe01@gmail.com>
2021-09-04 21:26:09 +0900

    item

---
+++ backend/app/schemas/item.py
@@ -0,0 +1,29 @@
+#更新時と追加時
+from typing import Optional
+from pydantic import BaseModel
+
+
+class ItemBase(BaseModel):
+    name: Optional[str] = None
+    image: Optional[str] = None
+    detail: Optional[str] = None
+    price: Optional[int] = None
+    url: Optional[str] = None
+
+
+class ItemCreate(ItemBase):
+    name: str
+    price: int
+
+
+class ItemUpdate(ItemBase):
+    pass
+
+class Item(ItemCreate):
+    id : int
+    image: str
+    detail: str
+    url: str
+
+    class Config:
+        orm_mode = True
