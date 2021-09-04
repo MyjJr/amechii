9a0a936f9440960ebffc3079a252efd1a0def9d8
Kuroi_Cc <ChenchengMiKuRo@gmail.com>
2021-09-04 15:04:45 +0900

    base user crud

--- backend/app/schemas/user.py
+++ backend/app/schemas/user.py
@@ -4,25 +4,30 @@ from pydantic import BaseModel
 
 
 class UserBase(BaseModel):
-    name: Optional[str] = None
+    display_name: Optional[str] = None
 
 
 class UserCreate(UserBase):
     name: str
+    icon: str = "default.png"
     password: str
 
 
 class UserUpdate(UserBase):
+    icon: str = "default.png"
     password: Optional[str] = None
-
-
-class UserLogin(UserCreate):
-    pass
 
 
 class User(UserBase):
     id: Optional[int] = None
+    name: Optional[int] = None
+    icon: str = "default.png"
     registration_time: Optional[datetime] = None
 
     class Config:
         orm_mode = True
+
+
+class UserLogin(BaseModel):
+    name: str
+    password: str
