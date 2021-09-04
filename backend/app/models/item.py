7ead51dcd1932a19a5a5ca1012b5f007555f7742
Kuroi_Cc <ChenchengMiKuRo@gmail.com>
2021-09-04 15:04:22 +0900

    comment out

--- backend/app/models/item.py
+++ backend/app/models/item.py
@@ -1,12 +1,12 @@
-from sqlalchemy import Column, Integer, String
+# from sqlalchemy import Column, Integer, String
 
-from app.db.base_class import Base
+# from app.db.base_class import Base
 
 
-class Item(Base):
-    __tablename__ = "items"
+# class Item(Base):
+#     __tablename__ = "items"
 
-    id = Column(Integer, primary_key=True, index=True, autoincrement=True)
-    name = Column(String(256), nullable=False)
-    image = Column(String(256))
-    price = Column(Integer, nullable=False)
+#     id = Column(Integer, primary_key=True, index=True, autoincrement=True)
+#     name = Column(String(256), nullable=False)
+#     image = Column(String(256))
+#     price = Column(Integer, nullable=False)
--- backend/app/models/task.py
+++ backend/app/models/task.py
@@ -1,51 +1,47 @@
-import datetime
-from typing import TYPE_CHECKING
+# import datetime
+# from typing import TYPE_CHECKING
 
-import enum
-from sqlalchemy.sql.sqltypes import Enum
-from sqlalchemy.orm import relationship
-from sqlalchemy import Column, Integer, String, DateTime, ForeignKey
+# import enum
+# from sqlalchemy.sql.sqltypes import Enum
+# from sqlalchemy.orm import relationship
+# from sqlalchemy import Column, Integer, String, DateTime, ForeignKey
 
-from app.db.base_class import Base
+# from app.db.base_class import Base
 
-if TYPE_CHECKING:
-    from app.models.user import User
+# if TYPE_CHECKING:
+#     from app.models.user import User
 
+# class Task(Base):
+#     __tablename__ = "tasks"
 
-class Task(Base):
-    __tablename__ = "tasks"
+#     id = Column(Integer, primary_key=True, index=True, autoincrement=True)
+#     user_id = Column(Integer, ForeignKey("users.id"), nullable=False)
+#     title = Column(String(256), index=True, nullable=False)
+#     create_datetime = Column(DateTime, default=datetime.datetime.now)
 
-    id = Column(Integer, primary_key=True, index=True, autoincrement=True)
-    user_id = Column(Integer, ForeignKey("users.id"), nullable=False)
-    title = Column(String(256), index=True, nullable=False)
-    create_datetime = Column(DateTime, default=datetime.datetime.now)
+#     subtasks = relationship("Subtask", back_populates="tasks")
+#     users = relationship("User", back_populates="tasks")
 
-    subtasks = relationship("Subtask", back_populates="tasks")
-    users = relationship("User", back_populates="tasks")
+# class PriorityType(str, enum.Enum):
+#     most = "2"
+#     more = "1"
+#     normal = "0"
 
-class PriorityType(str, enum.Enum):
-    most = "2"
-    more = "1"
-    normal = "0"
+# class StatusType(str, enum.Enum):
+#     not_complete = "not_complete"
+#     success = "success"
+#     faild = "faild"
 
+# class Subtask(Base):
+#     __tablename__ = "subtasks"
 
-class StatusType(str, enum.Enum):
-    not_complete = "not_complete"
-    success = "success"
-    faild = "faild"
+#     id = Column(Integer, primary_key=True, index=True, autoincrement=True)
+#     task_id = Column(Integer, ForeignKey("tasks.id"), nullable=False)
 
+#     title = Column(String(256), index=True, nullable=False)
+#     priority : Column(Enum(PriorityType), index=True, nullable=True)
+#     status : Column(Enum(StatusType), index=True, nullable=False)
+#     create_datetime = Column(DateTime, default=datetime.datetime.now)
+#     end_datetime = Column(DateTime)
 
-
-class Subtask(Base):
-    __tablename__ = "subtasks"
-
-    id = Column(Integer, primary_key=True, index=True, autoincrement=True)
-    task_id = Column(Integer, ForeignKey("tasks.id"), nullable=False)
-    
-    title = Column(String(256), index=True, nullable=False)
-    priority : Column(Enum(PriorityType), index=True, nullable=True)
-    status : Column(Enum(StatusType), index=True, nullable=False)
-    create_datetime = Column(DateTime, default=datetime.datetime.now)
-    end_datetime = Column(DateTime)
-
-    tasks = relationship("Task", back_populates="subtasks")
+#     tasks = relationship("Task", back_populates="subtasks")
