954120f82319fb348b2692e5a90778ebb0078379
Kuroi_Cc <ChenchengMiKuRo@gmail.com>
2021-09-09 13:14:00 +0900

    本番用データに切り替え

--- backend/app/db/db_curd_test.py
+++ backend/app/db/db_curd_test.py
@@ -25,7 +25,7 @@ def user_create(db):
 def follow_create(db):
     for i in demo_date.demo_follows:
         user = crud.user.follow(
-            db, from_user_id=i["from_user"], follow_user_id=i["to_user"]
+            db, from_user_id=i["from_id"], follow_user_id=i["to_id"]
         )
         # for i in user.following:
         #     print(i.name)
@@ -57,19 +57,16 @@ def address_create(db):
 
 
 def task_create(db):
-    item_id_list_in = []
-    for i in demo_date.demo_tasks_items:
-        item_id_list_in.append(i["item_id"])
-    for i in demo_date.demo_tasks:
+    for i in range(len(demo_date.demo_tasks)):
         task_in = TaskCreate(
-            set_id=i["from_user"],
-            do_id=i["to_user"],
-            address_id=i["addresses_id"],
-            title=i["title"],
-            deadline=i["deadline"],
-            back_money=i["back_money"],
-            status=i["status"],
-            item_id_list=item_id_list_in
+            set_id=demo_date.demo_tasks[i]["set_id"],
+            do_id=demo_date.demo_tasks[i]["do_id"],
+            address_id=demo_date.demo_tasks[i]["addresses_id"],
+            title=demo_date.demo_tasks[i]["title"],
+            deadline=demo_date.demo_tasks[i]["deadline"],
+            back_money=demo_date.demo_tasks[i]["back_money"],
+            status=demo_date.demo_tasks[i]["status"],
+            item_id_list=[demo_date.demo_tasks_items[i]["item_id"]]
         )
         task = crud.task.create(db, obj_in=task_in)
         print(task)
@@ -133,9 +130,10 @@ if __name__ == "__main__":
     session = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
     db: Session = session()
+    insert_demo_data_all(db)
 
-    a = crud.user.get_like_name(db, name="k")
-    a = 0
+    # a = crud.user.get_like_name(db, name="k")
+    # a = 0
     # b = crud.item.get(db, 1)
     # print_obj_attributes(b)
 
@@ -158,7 +156,6 @@ if __name__ == "__main__":
 
     # a = crud.transaction.create(db, obj_in=obj_in)
     # print(crud.transaction.get_balance(db, 1))
-    # insert_demo_data_all(db)
     # task_create(db)
 
     # task = crud.task.get(db, 1)
--- backend/app/db/demo_date.py
+++ backend/app/db/demo_date.py
@@ -2,52 +2,52 @@ import datetime
 
 demo_users = [
     {
-        "name": "kitty",
-        "display_name": "HelloKitty",
-        "icon": "kitty.jpeg",
+        "name": "hanako_y",
+        "display_name": "HANAKO",
+        "icon": "kitty.png",
         "password": "123456"
     }, {
-        "name": "mymelody",
-        "display_name": "マイメロディ",
-        "icon": "mymelo.jpeg",
+        "name": "kenta_m",
+        "display_name": "KENTA",
+        "icon": "mymelo.png",
         "password": "1234561"
     }, {
-        "name": "cinnamoroll",
-        "display_name": "シナモロール",
-        "icon": "cinnamon.jpeg",
+        "name": "rina_m",
+        "display_name": "RINA",
+        "icon": "cinnamon.png",
         "password": "1234567"
     }, {
-        "name": "pomurin",
-        "display_name": "ポムポムプリン",
-        "icon": "pomu.jpeg",
+        "name": "ryo_tnk",
+        "display_name": "RYO",
+        "icon": "pomu.png",
         "password": "1234568"
     }
 ]
 
-kitty_id = 2
-mymelo_id = 3
-cinnamon_id = 4
-pomu_id = 5
+hanako_id = 2
+kenta_id = 3
+rina_id = 4
+ryo_id = 5
 
 demo_follows = [
     {
-        "from_user": kitty_id,
-        "to_user": mymelo_id
+        "from_id": hanako_id,
+        "to_id": kenta_id
     }, {
-        "from_user": mymelo_id,
-        "to_user": cinnamon_id
+        "from_id": kenta_id,
+        "to_id": rina_id
     }, {
-        "from_user": kitty_id,
-        "to_user": cinnamon_id
+        "from_id": hanako_id,
+        "to_id": rina_id
     }, {
-        "from_user": cinnamon_id,
-        "to_user": pomu_id
+        "from_id": rina_id,
+        "to_id": ryo_id
     }, {
-        "from_user": pomu_id,
-        "to_user": kitty_id
+        "from_id": ryo_id,
+        "to_id": hanako_id
     }, {
-        "from_user": pomu_id,
-        "to_user": cinnamon_id
+        "from_id": ryo_id,
+        "to_id": rina_id
     }
 ]
 
@@ -55,132 +55,173 @@ demo_transactions = [
     {
         "amount": 22000,
         "net_balance": 22000,
-        "user_id": kitty_id
+        "user_id": hanako_id
     }, {
-        "amount": -2000,
-        "net_balance": 20000,
-        "user_id": kitty_id
+        "amount": -20000,
+        "net_balance": 2000,
+        "user_id": hanako_id
     }, {
         "amount": 21000,
         "net_balance": 21000,
-        "user_id": mymelo_id
+        "user_id": kenta_id
     }, {
-        "amount": -1000,
-        "net_balance": 20000,
-        "user_id": mymelo_id
+        "amount": -10000,
+        "net_balance": 11000,
+        "user_id": kenta_id
     }, {
         "amount": 31000,
         "net_balance": 31000,
-        "user_id": cinnamon_id
+        "user_id": rina_id
     }, {
         "amount": -1000,
         "net_balance": 30000,
-        "user_id": cinnamon_id
+        "user_id": rina_id
     }, {
         "amount": 55000,
         "net_balance": 55000,
-        "user_id": pomu_id
+        "user_id": ryo_id
     }, {
-        "amount": -5000,
-        "net_balance": 50000,
-        "user_id": pomu_id
+        "amount": -50000,
+        "net_balance": 5000,
+        "user_id": ryo_id
     }
 ]
 
 demo_items = [
     {
-        "name": "にんじん",
-        "image": "carrot.jpeg",
-        "detail": "新鮮なにんじん",
-        "price": 2000,
-    }, {
-        "name": "トマト",
-        "image": "tomato.jpeg",
-        "detail": "美味しいトマト",
+        "name": "時計",
+        "image": "watch.jpeg",
+        "detail": "クールな時計",
+        "price": 20000,
+    },
+    {
+        "name": "ゲーム",
+        "image": "game.jpeg",
+        "detail": "新作のゲーム",
+        "price": 10000,
+    },
+    {
+        "name": "ぬいぐるみ",
+        "image": "bear.jpeg",
+        "detail": "可愛いぬいぐるみ",
         "price": 1000,
-    }, {
-        "name": "ベリー",
-        "image": "berry.jpeg",
-        "detail": "甘いベリー",
-        "price": 1000,
-    }, {
-        "name": "バナナ",
-        "image": "banana.jpeg",
-        "detail": "フィリピンのバナナ",
+    },
+    {
+        "name": "タブレット",
+        "image": "tablet.jpeg",
+        "detail": "便利なタブレット",
+        "price": 50000,
+    },
+    {
+        "name": "ギター",
+        "image": "guitar.jpeg",
+        "detail": "人気No.1のギター",
+        "price": 20000,
+    },
+    {
+        "name": "アイシャドウ",
+        "image": "cosme.jpeg",
+        "detail": "肌に優しいアイシャドウ",
         "price": 5000,
-    }
+    },
+    {
+        "name": "指輪",
+        "image": "ring.jpeg",
+        "detail": "ゴールドのリング",
+        "price": 10000,
+    },
+    {
+        "name": "ワイン",
+        "image": "wine.jpeg",
+        "detail": "人気のワイン",
+        "price": 10000,
+    },
+    {
+        "name": "ゲーム",
+        "image": "game_2.jpeg",
+        "detail": "限定色のゲーム機",
+        "price": 30000,
+    },
+    {
+        "name": "カメラ",
+        "image": "camera.jpeg",
+        "detail": "質の良いカメラ",
+        "price": 40000,
+    },
+    {
+        "name": "サングラス",
+        "image": "glasses.jpeg",
+        "detail": "オシャレなサングラス",
+        "price": 10000,
+    },
 ]
 
 demo_addresses = [
     {
-        "name": "キティ",
+        "name": "HANAKO",
         "phone_number": "080-0000-0000",
         "zipcode": "000-0000",
-        "address": "ハロー県キティ市2-5",
-        "user_id": kitty_id
+        "address": "◯◯県△△市2-5",
+        "user_id": hanako_id
     }, {
-        "name": "マイメロディ",
+        "name": "KENTA",
         "phone_number": "090-0000-0000",
         "zipcode": "222-2222",
-        "address": "マイメロディタウン25-25",
-        "user_id": mymelo_id
+        "address": "◯◯県△△市20-5",
+        "user_id": kenta_id
     }, {
-        "name": "シナモロール",
+        "name": "RINA",
         "phone_number": "090-1111-1111",
         "zipcode": "111-1111",
-        "address": "シナモロールカフェ4-7",
-        "user_id": cinnamon_id
+        "address": "◯◯県△△市4-7",
+        "user_id": rina_id
     }, {
-        "name": "ポムポムプリン",
+        "name": "RYO",
         "phone_number": "080-1111-1111",
         "zipcode": "333-3333",
-        "address": "ポムポム府プリン市2-5",
-        "user_id": pomu_id
+        "address": "◯◯県△△市7-5",
+        "user_id": ryo_id
     }
 ]
 
-kitty_address_id = 1
-mymelo_address_id = 2
-cinnamon_address_id = 3
-pomu_address_id = 4
+hanako_address_id = 1
+kenta_address_id = 2
+rina_address_id = 3
+ryo_address_id = 4
 
 demo_tasks = [
     {
         "title": "買い物に行く",
-        "priority": 0,
-        "deadline": datetime.datetime(2021, 10, 1, 15, 30),
+        "deadline": datetime.datetime(2021, 10, 1, 12, 00),
         "status": "not_complete",
         "back_money": True,
-        "addresses_id": kitty_address_id,
-        "from_user": mymelo_id,
-        "to_user": kitty_id
+        "set_id": kenta_id,
+        "do_id": hanako_id,
+        "addresses_id": hanako_address_id
     }, {
         "title": "家事をする",
-        "priority": 1,
-        "deadline": datetime.datetime(2021, 12, 2, 15, 30),
+        "deadline": datetime.datetime(2021, 10, 10, 23, 00),
         "status": "faild",
         "back_money": True,
-        "addresses_id": mymelo_address_id,
-        "from_user": pomu_id,
-        "to_user": mymelo_id
+        "set_id": rina_id,
+        "do_id": kenta_id,
+        "addresses_id": kenta_address_id
     }, {
         "title": "合格する",
-        "priority": 2,
-        "deadline": datetime.datetime(2021, 12, 1, 15, 40),
+        "deadline": datetime.datetime(2021, 11, 22, 15, 40),
         "status": "success",
         "back_money": True,
-        "addresses_id": cinnamon_address_id,
-        "from_user": kitty_id,
-        "to_user": cinnamon_id
+        "set_id": hanako_id,
+        "do_id": rina_id,
+        "addresses_id": rina_address_id
     }, {
         "title": "痩せる",
-        "priority": 1,
-        "deadline": datetime.datetime(2021, 11, 2, 15, 30),
+        "deadline": datetime.datetime(2021, 12, 12, 15, 40),
         "status": "not_complete",
         "back_money": False,
-        "addresses_id": pomu_address_id,
-        "from_user": pomu_id,
-        "to_user": pomu_id
+        "set_id": ryo_id,
+        "do_id": ryo_id,
+        "addresses_id": ryo_address_id
     }
 ]
 
@@ -228,39 +269,39 @@ demo_subtasks = [
     }
 ]
 
-item_carrot_id = 1
-item_tomato_id = 2
-item_berry_id = 3
-item_banana_id = 4
+item_watch_id = 1
+item_game_id = 2
+item_bear_id = 3
+item_tablet_id = 4
 
 demo_tasks_items = [
     {
         "task_id": shopping_task,
-        "item_id": item_carrot_id
+        "item_id": item_watch_id
     }, {
         "task_id": house_task,
-        "item_id": item_tomato_id
+        "item_id": item_game_id
     }, {
         "task_id": pass_task,
-        "item_id": item_berry_id
+        "item_id": item_bear_id
     }, {
         "task_id": diet_task,
-        "item_id": item_banana_id
+        "item_id": item_tablet_id
     }
 ]
 
 demo_favourite = [
     {
-        "user_id": kitty_id,
-        "item_id": item_carrot_id
+        "user_id": kenta_id,
+        "item_id": item_watch_id
     }, {
-        "user_id": mymelo_id,
-        "item_id": item_tomato_id
+        "user_id": rina_id,
+        "item_id": item_game_id
     }, {
-        "user_id": cinnamon_id,
-        "item_id": item_berry_id
+        "user_id": hanako_id,
+        "item_id": item_bear_id
     }, {
-        "user_id": kitty_id,
-        "item_id": item_banana_id
+        "user_id": ryo_id,
+        "item_id": item_tablet_id
     }
 ]
