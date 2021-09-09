8c268a7f0e6044cb25bf28d41c54769870f96e33
TaikiCode <78462662+TaikiCode@users.noreply.github.com>
2021-09-09 17:14:56 +0900

    仮のユーザー情報取得

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -10,32 +10,6 @@ export default function Home() {
 
   const cookies = new Cookies();
 
-  // useEffect(() => {
-  //   setTokenInfo({
-  //     access_token: cookies.get("access_token"),
-  //     token_type: cookies.get("token_type"),
-  //   });
-  // }, [cookies]);
-
-
-  useEffect( async () => {
-      const res = await fetch(`${process.env.NEXT_PUBLIC_RESTAPI_URL}api/v1/users/get-info`,
-      {
-        method: "GET",
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `${cookies.get("token_type")} ${cookies.get("access_token")}`,
-        }
-      }).then((res) => {
-        if(res.status === 401) {
-          alert("Token not valid");
-        }
-      })
-      console.log(res.json())
-      // return res.json()
-  }, [])
-
-
   return (
     <div className="layout-container">
       <Head>
