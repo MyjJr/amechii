162a758297eea6b6bcd908abea929723348ae288
TaikiCode <78462662+TaikiCode@users.noreply.github.com>
2021-09-09 14:42:32 +0900

    fetch

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -7,8 +7,34 @@ import Cookies from "universal-cookie";
 import Navbar from "../components/Navbars/Navbar";
 
 export default function Home() {
-  // cookieインスタンス
-  // const cookies = new Cookies();
+
+  const cookies = new Cookies();
+
+  // useEffect(() => {
+  //   setTokenInfo({
+  //     access_token: cookies.get("access_token"),
+  //     token_type: cookies.get("token_type"),
+  //   });
+  // }, [cookies]);
+
+
+  useEffect( async () => {
+      const res = await fetch(`${process.env.NEXT_PUBLIC_RESTAPI_URL}api/v1/users/get-info`,
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `${cookies.get("token_type")} ${cookies.get("access_token")}`,
+        }
+      }).then((res) => {
+        if(res.status === 401) {
+          alert("Token not valid");
+        }
+      })
+      console.log(res.json())
+      // return res.json()
+  }, [])
+
 
   return (
     <div className="layout-container">
