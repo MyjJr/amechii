8902aeedb854346666c3b4a3a45c547efbbffb7e
Kuroi_Cc <ChenchengMiKuRo@gmail.com>
2021-09-11 05:17:32 +0900

    add category to item

--- backend/app/db/base.py
+++ backend/app/db/base.py
@@ -3,3 +3,4 @@ from app.models.user import User, Transaction, Favourite  # noqa
 from app.models.item import Item  # noqa
 from app.models.task import Task, SubTask, Task_item  # noqa
 from app.models.address import Address  # noqa
+from app.models.prepaid_card import PrepaidCard  # noqa
